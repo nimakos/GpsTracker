e382e434fdeee5aeb858d7bd6cdb23984608637a
nimakos <nimakos@hotmail.com>
2020-01-21 12:26:24 +0200

    androidX

--- app/src/main/java/gr/invision/gpstracker/db/databaseHolder/AppDatabase.java
+++ app/src/main/java/gr/invision/gpstracker/db/databaseHolder/AppDatabase.java
@@ -1,8 +1,8 @@
 package gr.invision.gpstracker.db.databaseHolder;
 
-import android.arch.persistence.room.Database;
-import android.arch.persistence.room.Room;
-import android.arch.persistence.room.RoomDatabase;
+import androidx.room.Database;
+import androidx.room.Room;
+import androidx.room.RoomDatabase;
 import android.content.Context;
 
 import gr.invision.gpstracker.db.dataAccessObject.GpsDao;
--- app/src/main/java/gr/invision/gpstracker/db/entity/GpsRecord.java
+++ app/src/main/java/gr/invision/gpstracker/db/entity/GpsRecord.java
@@ -1,8 +1,8 @@
 package gr.invision.gpstracker.db.entity;
 
-import android.arch.persistence.room.ColumnInfo;
-import android.arch.persistence.room.Entity;
-import android.arch.persistence.room.PrimaryKey;
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
 
 import com.thoughtworks.xstream.annotations.XStreamAlias;
 
